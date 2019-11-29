3cef979e10c33ff4441be8a4c84f40f9ebabcb62
Brian Langer <brian@supracoelum.com>
2019-11-29 17:39:12 +1300

    - updated test - added repository link to package

--- tests/initializationTests.js
+++ tests/initializationTests.js
@@ -2,12 +2,20 @@
 const {Astrologer, TimeConversions} = require('../distribution/index.js');
 var moment = require('moment-timezone');
 
-test();
+testPlanetCalculation();
+testTimeConversion();
 
-async function test ()
+async function testPlanetCalculation ()
 {
     let astrologer = new Astrologer();
     await astrologer.Init();
     let planets = astrologer.CalculateCelestialBodiesAndTime(moment(),"GB",{latitude:33,longitude:55});
     console.log(JSON.stringify(planets));
+}
+
+function testTimeConversion()
+{
+    let timeConversions = new TimeConversions();
+    let result = timeConversions.ConvertDecimalDegreesToHMS(21.8);
+    console.log(JSON.stringify(result));
 }
